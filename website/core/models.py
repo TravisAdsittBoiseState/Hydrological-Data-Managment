15fbe6f426c131c9dd7bf71985ccd0989e242573
Trevor Ohlinger <trevorohlinger@u.boisestate.edu>
2018-10-24 09:31:50 -0600

    Added in code for ER diagrams being displayed - not working yet.

--- website/core/models.py
+++ website/core/models.py
@@ -12,7 +12,27 @@ class DataRequest(models.Model):
     name = models.CharField(max_length=255)
     url = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
-
+	
+class Image(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.CharField(max_length=1000)
+	image = models.FileField(upload_to='diagrams')
+	
+	def image_tag(self):
+		return mark_safe('<img src="\website\core\static\admin\images\ER-Diagram.png" />')
+	
+	image_tag.short_description = 'ER-Diagram'
+	
+	def __str__(self):
+		return self.title
+		
+class Category(models.Model):
+	Image = models.ForeignKey(Image, on_delete=models.CASCADE)
+	name = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.name	
+	
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	if created: # a new record was created
